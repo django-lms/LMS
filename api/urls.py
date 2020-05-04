1d9da03139dd98c14794fee8fce93b71ab99f484
Sebastian Savino <seba.manda98@gmail.com>
2020-05-04 18:29:41 -0300

    Change ID to Course Name

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 
 from api.views.teachers import TeacherAPIView
 from api.views.students import StudentAPIView,StudentDetailView
-from api.views.courses import SectionAPIView
+from api.views.courses import SectionAPIView, SectionDetailview
 
 
 urlpatterns = [
     path('teachers/', TeacherAPIView.as_view()),
     path('students/', StudentAPIView.as_view()),
     path('students/<code>', StudentDetailView.as_view()),
-    path('sections/', SectionAPIView.as_view()),
+    path('courses/sections/', SectionAPIView.as_view()),
+    path('courses/sections/<int:pk>/', SectionDetailview.as_view()),
 ]
--- api/views/courses.py
+++ api/views/courses.py
@@ -3,6 +3,13 @@ from rest_framework import generics
 from core.courses.models import Section
 from api.serializers.courses import SectionSerializers
 
+
 class SectionAPIView(generics.ListAPIView):
     queryset = Section.objects.all()
     serializer_class = SectionSerializers
+
+
+class SectionDetailview(generics.RetrieveAPIView):
+    queryset=Section.objects.all()
+    serializer_class = SectionSerializers
+
